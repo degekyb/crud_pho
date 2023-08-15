f837e7230945c0b49a28ccd0ee545642544b8a86
degekyb <alvesdasilvadiego8@gmail.com>
2023-08-14 22:16:15 -0300

    first commit

---
+++ conexao/conexao.php
@@ -0,0 +1,25 @@
+<?php
+    
+    class Database{
+        private $host = "localhost";
+        private $db_name = "aula3crud";
+        private $username = "root";
+        private $senha = "";
+        public $conn;
+
+        public function getConnection(){
+            $this->conn = null;
+
+            try{
+                $this->conn = new PDO("mysql:host=". $this->host.";dbname=" . $this->db_name, $this->username, $this->senha);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            }catch(PDOException $e){
+                echo "Erro na conexao: ". $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+    }
+
+?>
